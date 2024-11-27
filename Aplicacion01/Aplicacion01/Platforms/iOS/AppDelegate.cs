686b7b6117f2476ab19cc6faf4b54d2a9228371b
Juan Rodriguez <juanrodrigueza@protonmail.com>
2024-11-27 19:15:40 -0300

    Primer commit

---
+++ Aplicacion01/Aplicacion01/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Aplicacion01
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
